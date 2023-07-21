2d267efa77ef3e6341f0d4f41e317e1af6c108e8
Barbaro Javier <barbaro.valmaseda@gmail.com>
2023-07-21 16:46:53 +0400

    completed all task

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 // Ejercicio 2
-async function writeFile (filePath, data, callback) {
+const path = require("node:path");
+const fs = require("node:fs");
 
+async function writeFile(filePath, data, callback) {
+  const directoryPath = path.dirname(filePath);
+
+  fs.mkdir(directoryPath, { recursive: true }, () => {
+    fs.writeFile(filePath, data, { recursive: true }, () => callback());
+  });
+}
+
+function countWordInText(word, text) {
+  const regex = new RegExp(word, "gi");
+  const founds = text.match(regex);
+  return founds ? founds.length : 0;
 }
 
 // Ejercicio 3
-async function readFileAndCount (word, callback) {
+async function readFileAndCount(word, callback) {
+  const filePath = process.argv[2];
+  if (!word) {
+    callback(new Error("No se ha especificado la palabra a buscar"));
+    return;
+  }
 
+  if (!filePath) {
+    callback(new Error("No se ha especificado el path del archivo"));
+    return;
+  }
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.log(err.code);
+      if (err.code === "ENOENT") {
+        callback(null, 0);
+      } else {
+        callback(err);
+      }
+      return;
+    }
+
+    const occurrences = countWordInText(word, data);
+    callback(null, occurrences);
+  });
 }
 
 module.exports = {
   writeFile,
-  readFileAndCount
-}
+  readFileAndCount,
+};
